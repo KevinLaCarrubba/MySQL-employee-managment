b985eb3e66f4e166e0a71c5c79a04dde3f60745a
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-09-15 10:01:28 -0400

    pw delete

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   // Your username
   user: "root",
   // Your password
-  password: "71Niveklac",
+  password: "password",
   database: "employees",
 });
 
